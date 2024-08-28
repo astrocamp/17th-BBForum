621b21460647749d8a071c9c7f3be44b921b697a
erichsu <filafila0521@gmail.com>
2024-08-28 23:26:46 +0800

    feat: add pages app and complete the navigation bar in index.html

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -1,6 +1,6 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import { faBookmark as fasBookmark } from "@fortawesome/free-solid-svg-icons"
+import { faBookmark as fasBookmark, faMagnifyingGlass as fasMagnifyingGlass, faBell as fasBell } from "@fortawesome/free-solid-svg-icons"
 import { faBookmark as farBookmark } from "@fortawesome/free-regular-svg-icons"
 
-library.add(farBookmark, fasBookmark)
+library.add(farBookmark, fasBookmark, fasMagnifyingGlass, fasBell)
 dom.i2svg()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,py}"],
+  content: [
+    "./**/*.{html,js}",
+    './templates/**/*.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-800': '#363636',
+        'gray-90': '#e5e5e5',
+        'red-primary': '#ae2024',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
