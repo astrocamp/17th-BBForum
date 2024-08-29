d71ec47fd4dfd829727c494d2540a4cfb5acaa0b
erichsu <filafila0521@gmail.com>
2024-08-29 19:46:07 +0800

    feat: add right aside in main page

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -1,6 +1,6 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import { faBookmark as fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage} from "@fortawesome/free-solid-svg-icons"
+import { faBookmark as fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus} from "@fortawesome/free-solid-svg-icons"
 import { faBookmark as farBookmark } from "@fortawesome/free-regular-svg-icons"
 
-library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage)
+library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus)
 dom.i2svg()
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,17 @@ module.exports = {
       colors: {
         'dark-600': '#676767',
         'dark-800': '#363636',
+        'gray-221': '#dddddd',
         'gray-69': '#b0b0b0',
         'gray-83': '#d4d4d4',
         'gray-90': '#e5e5e5',
+        'gray-94': '#f0f0f0',
         'red-primary': '#ae2024',
+        'red-900': '#990000',
       },
       boxShadow: {
         'nav-bar': '0 1px 8px 0 rgba(0, 0, 0, .2)',
+        'aside-right': '0 1px 1px rgba(0, 0, 0, .15), -1px 0 0 rgba(0, 0, 0, .03), 1px 0 0 rgba(0, 0, 0, .03), 0 1px 0 rgba(0, 0, 0, .12)',
       },
     },
     fontFamily: {
