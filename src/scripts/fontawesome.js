e5bd214e83a70b3fcb25d24d9f6c0e90a8761e79
erichsu <filafila0521@gmail.com>
2024-08-29 14:58:07 +0800

    feat: add aside page in main page

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -1,6 +1,6 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import { faBookmark as fasBookmark, faMagnifyingGlass as fasMagnifyingGlass, faBell as fasBell } from "@fortawesome/free-solid-svg-icons"
+import { faBookmark as fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage} from "@fortawesome/free-solid-svg-icons"
 import { faBookmark as farBookmark } from "@fortawesome/free-regular-svg-icons"
 
-library.add(farBookmark, fasBookmark, fasMagnifyingGlass, fasBell)
+library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage)
 dom.i2svg()
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'dark-600': '#676767',
         'dark-800': '#363636',
+        'gray-69': '#b0b0b0',
+        'gray-83': '#d4d4d4',
         'gray-90': '#e5e5e5',
         'red-primary': '#ae2024',
       },
@@ -15,6 +18,9 @@ module.exports = {
         'nav-bar': '0 1px 8px 0 rgba(0, 0, 0, .2)',
       },
     },
+    fontFamily: {
+      'sans': ['Arial', 'Helvetica', 'sans-serif'],
+    }
   },
   plugins: [
     require('daisyui'),
