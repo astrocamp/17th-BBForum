f5117da90774133904cbe3b74a2cec2d7d10ef55
emma0620 <emmawang0620@gmail.com>
2024-08-29 11:15:17 +0800

    feat: add model- profile and connect with User

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 # 只有在開發環境需要這兩個套件
@@ -110,20 +111,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
@@ -141,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
---
+++ users/views.py
@@ -0,0 +1,71 @@
+
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.contrib.auth import authenticate
+from .forms import ProfileForm, UserForm
+from .models import Profile
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+def index(req):
+
+    if req.method == "POST":
+        form = ProfileForm(req.POST)
+        #user = authenticate(User.id=2,username=username)
+        if form.is_valid():
+            user = User.objects.get(id=1)
+            # 避免不必要的數據庫操作：如果表單中包含許多字段，並且你只想更新部分字段或添加額外的邏輯，這樣可以避免不必要的數據庫操作。
+            profile = form.save(commit=False) 
+            profile.user = user  # 設置 user 字段
+            # 使用 update_or_create 來創建或更新 profile
+            profile, created = Profile.objects.update_or_create(
+             user=user,  # 查找的條件
+             defaults={
+            'nickname': profile.nickname,
+            'gender': profile.gender,
+            'birthday': profile.birthday,
+            'location': profile.location,
+            'education': profile.education,
+            'Investment_experience': profile.Investment_experience,
+            'Investment_tool': profile.Investment_tool,
+            'investment_attributes': profile.investment_attributes,
+             }
+            )
+            profile.save()
+            return redirect(reverse("users:index"))
+        else:
+            print(form.errors)
+            # return render(req, "users/new.html", {"form": form})
+    posts = Profile.objects.all()
+    return render(req, "users/index.html", {"posts": posts} )
+
+
+def show(req, id):
+    post = get_object_or_404(Profile, pk=id)
+    if req.method == "POST":
+        form = ProfileForm(req.POST, instance=post)
+        if form.is_valid():
+            form.save()
+        else:
+            return render(req, "users/edit.html", {"form": form, "post": post})
+    return render(req, "users/show.html", {"post": post})
+
+
+def new(req):
+    form = ProfileForm()
+    return render(req, "users/new.html", {"form": form})
+
+
+def edit(req, id):
+    post = get_object_or_404(Profile, pk=id)
+    form = ProfileForm(instance=post)
+    return render(req, "users/edit.html", {"form": form, "post": post})
+
+
+def delete(req, id):
+    post = get_object_or_404(User, pk=id)
+    post.delete()
+    return redirect("users:index")
+
