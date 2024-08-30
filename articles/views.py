0e51dfbc6095896748c7e5f81860d7f733bc405c
emma0620 <emmawang0620@gmail.com>
2024-08-30 17:49:11 +0800

    feat: add article CRUD (include softdelete and photo loading saved at local temporary)

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.contrib.auth import authenticate
+from .forms import ArticleForm
+from .models import Article
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your views here.
+
+
+def index(req):
+    if req.method == "POST":
+       form = ArticleForm(req.POST, req.FILES)  
+       # 注意如果表單中有文件上傳，應該使用 req.FILES
+       if form.is_valid():
+            user = get_object_or_404(User, id=4)            
+            # 避免不必要的數據庫操作：保存表單數據但不提交到數據庫
+            article = form.save(commit=False)
+            article.userID = user  # 設置 user 字段
+            article.post_at = timezone.now()  # 設置創建時間            
+            article.save()  # 保存到數據庫
+            
+            return redirect(reverse("articles:index"))
+       else:
+            return render(req, "articles/new.html", {"form": form})
+    posts = Article.objects.all()
+    return render(req, "articles/index.html", {"posts": posts})
+
+
+def show(req, id):
+    post = get_object_or_404(Article, pk=id)
+    if req.method == "POST":
+        form = ArticleForm(req.POST, instance=post)
+        if form.is_valid():
+            form.save()
+        else:
+            return render(req, "articles/edit.html", {"form": form, "post": post})
+    return render(req, "articles/show.html", {"post": post})
+
+
+def new(req):
+    form = ArticleForm()
+    return render(req, "articles/new.html", {"form": form})
+
+
+def edit(req, id):
+    post = get_object_or_404(Article, pk=id)
+    form = ArticleForm(instance=post)
+    return render(req, "articles/edit.html", {"form": form, "post": post})
+
+
+def delete(req, id):
+    post = get_object_or_404(Article, pk=id)
+    post.delete()
+    return redirect("articles:index")
