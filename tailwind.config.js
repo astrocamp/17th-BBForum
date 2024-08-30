afcb7391a7c7963b0d5f327ae21909206f3c9108
erichsu <filafila0521@gmail.com>
2024-08-30 13:01:01 +0800

    feat: add middle section in main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-600': '#676767',
         'dark-800': '#363636',
+        'dark-600': '#676767',
         'gray-221': '#dddddd',
-        'gray-69': '#b0b0b0',
-        'gray-83': '#d4d4d4',
-        'gray-90': '#e5e5e5',
         'gray-94': '#f0f0f0',
+        'gray-90': '#e5e5e5',
+        'gray-83': '#d4d4d4',
+        'gray-69': '#b0b0b0',
+        'gray-56': '#8f8f8f',
         'red-primary': '#ae2024',
         'red-900': '#990000',
       },
       boxShadow: {
         'nav-bar': '0 1px 8px 0 rgba(0, 0, 0, .2)',
-        'aside-right': '0 1px 1px rgba(0, 0, 0, .15), -1px 0 0 rgba(0, 0, 0, .03), 1px 0 0 rgba(0, 0, 0, .03), 0 1px 0 rgba(0, 0, 0, .12)',
+        'area': '0 1px 1px rgba(0, 0, 0, .15), -1px 0 0 rgba(0, 0, 0, .03), 1px 0 0 rgba(0, 0, 0, .03), 0 1px 0 rgba(0, 0, 0, .12)',
       },
     },
     fontFamily: {
