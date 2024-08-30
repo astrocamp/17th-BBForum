a1a6fbdafae4f52c8174bb8fdf7743d3c07c8d18
KANG <k306221@gmail.com>
2024-08-30 20:07:49 +0800

    fix: 修改layouts檔名、以及相關路徑

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,6 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path("admin@bbforum.17th/", admin.site.urls),
     path("social-auth/", include("social_django.urls", namespace="social")),
