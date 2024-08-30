c4d8687f6622ba93af4a47480af2ba7093106d9f
erichsu <filafila0521@gmail.com>
2024-08-30 13:01:01 +0800

    feat: add middle section in main page

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -1,6 +1,6 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import { faBookmark as fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus} from "@fortawesome/free-solid-svg-icons"
-import { faBookmark as farBookmark } from "@fortawesome/free-regular-svg-icons"
+import { faBookmark as fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus, faAngleRight, faSortDown, faEllipsis, faShare} from "@fortawesome/free-solid-svg-icons"
+import { faBookmark as farBookmark, faComment, faThumbsUp } from "@fortawesome/free-regular-svg-icons"
 
-library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus)
+library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus, faAngleRight, faSortDown, faEllipsis, faShare, faComment, faThumbsUp)
 dom.i2svg()
