8f4e781156618f0ebceffcf3a653e168e609b4aa
erichsu <filafila0521@gmail.com>
2024-09-02 23:54:06 +0800

    feat: 增加在登出狀態無法追蹤的權限

--- src/scripts/components/unlogined_prompt.js
+++ src/scripts/components/unlogined_prompt.js
@@ -3,11 +3,12 @@ import Alpine from "alpinejs"
 Alpine.data("unlogined_prompt", (isAuthenticated) => ({
     isVisible: false,
     isReadonly: false,
+    isShow: false,
     isAuthenticated: isAuthenticated,
 
     toggleVisibility() {
         this.isVisible = !this.isVisible;
         this.isReadonly = !this.isReadonly;
-    }
-
+        this.isShow = !this.isShow;
+    },
 }))
