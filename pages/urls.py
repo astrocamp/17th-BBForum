77f11a31309c047006360e63ad94a141c9d09207
erichsu <filafila0521@gmail.com>
2024-09-03 14:32:39 +0800

    feat: 增加會員資料選單連結到新的頁面

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,5 @@ app_name = "pages"
 urlpatterns = [
     path("", views.index, name="index"),
     path("my_watchlist/", views.my_watchlist, name="my_watchlist"),
+    path("member_profile/", views.member_profile, name="member_profile"),
 ]
