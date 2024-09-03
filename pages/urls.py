36e2cfac9b5f1bcb97c2c12efda64646eb777363
erichsu <filafila0521@gmail.com>
2024-09-03 16:10:02 +0800

    feat: 增加我的收藏頁面

--- pages/urls.py
+++ pages/urls.py
@@ -7,5 +7,6 @@ app_name = "pages"
 urlpatterns = [
     path("", views.index, name="index"),
     path("my_watchlist/", views.my_watchlist, name="my_watchlist"),
+    path("my_favorites/", views.my_favorites, name="my_favorites"),
     path("member_profile/", views.member_profile, name="member_profile"),
 ]
