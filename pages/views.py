546dbd72cdd18642d18e51fc804affdc91af0e03
erichsu <filafila0521@gmail.com>
2024-09-03 14:32:39 +0800

    feat: 增加會員資料選單連結到新的頁面

--- pages/views.py
+++ pages/views.py
@@ -8,3 +8,7 @@ def index(req):
 
 def my_watchlist(req):
     return render(req, "pages/my_watchlist/my_watchlist.html")
+
+
+def member_profile(req):
+    return render(req, "pages/nav_page/member_profile.html")
