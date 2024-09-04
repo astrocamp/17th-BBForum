033c4ec9b471ba2fb490da5bf92f1a201a0aef57
erichsu <filafila0521@gmail.com>
2024-09-04 12:13:07 +0800

    feat: 新增大盤和台指期頁面

--- pages/urls.py
+++ pages/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("my_watchlist/", views.my_watchlist, name="my_watchlist"),
     path("my_favorites/", views.my_favorites, name="my_favorites"),
     path("news_feed/", views.news_feed, name="news_feed"),
+    path("market_index/", views.market_index, name="market_index"),
+    path("taiwan_index/", views.taiwan_index, name="taiwan_index"),
     path("member_profile/", views.member_profile, name="member_profile"),
 ]
