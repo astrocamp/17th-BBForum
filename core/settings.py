d79b78dc24402597dc1ce73b3ca4e0438e30c612
jjs <jenjingsu860630@gmail.com>
2024-09-05 20:04:49 +0800

    fix: 新增popular的app

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,10 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [
+            BASE_DIR / "templates",
+            BASE_DIR / "populars/templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
---
+++ pages/templatetags/url_redirect.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def should_include_nav_bar(context):
+    request = context['request']
+    url_name = request.resolver_match.url_name
+    path = request.path
+    if url_name not in ['register', 'sign_in', 'auth_denied'] and path in ['/', '/my_watchlist/', '/my_favorites/', '/news_feed/', '/market_index/', '/taiwan_index/', '/popular_stocks/', '/popular_students/', '/popular_answers/']:
+        return True
+    return False
+
