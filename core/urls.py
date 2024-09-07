2759b0485f56feaf94447485074933a3ff46d64c
jjs <jenjingsu860630@gmail.com>
2024-09-07 16:18:51 +0800

    fix: 會員中心、然後轉跳到我的自選股

--- core/urls.py
+++ core/urls.py
@@ -6,24 +6,24 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 from django.urls import include, path
 
+
 def is_dev():
     return settings.DEBUG
-urlpatterns = (
-    [
-        path("social-auth/", include("social_django.urls", namespace="social")),
-        path("", include("pages.urls"), name="pages"),
-        path("users/", include("users.urls"), name="users"),
-        path("userprofiles/", include("userprofiles.urls"), name="userprofiles"),
-        path("articles/", include("articles.urls")),
-        path("points/", include("points.urls"), name="points"),
-        path("populars/", include(("populars.urls", "populars"), namespace="popular_pages")),
-    ]
-)
+
+
+urlpatterns = [
+    path("social-auth/", include("social_django.urls", namespace="social")),
+    path("", include("pages.urls"), name="pages"),
+    path("users/", include("users.urls"), name="users"),
+    path("userprofiles/", include("userprofiles.urls"), name="userprofiles"),
+    path("articles/", include("articles.urls")),
+    path("points/", include("points.urls"), name="points"),
+    path(
+        "populars/", include(("populars.urls", "populars"), namespace="popular_pages")
+    ),
+]
 
 if is_dev():
     urlpatterns += debug_toolbar_urls()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
