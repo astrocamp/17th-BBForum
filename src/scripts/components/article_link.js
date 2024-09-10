f6935c5db687a1c27173011b54cd753be910ad35
emma0620 <emmawang0620@gmail.com>
2024-09-10 11:45:59 +0800

    fix: 優化"複製連結"訊息框

---
+++ src/scripts/components/article_link.js
@@ -0,0 +1,27 @@
+import Alpine from "alpinejs"
+
+Alpine.data('shareLinkComponent', () => ({
+    showTooltip: false,
+    copied: false,
+    url: '',
+
+    init() {
+        // 從 data-url 屬性中讀取 URL
+        const urlElement = this.$el.dataset.url;
+        if (urlElement) {
+            this.url = urlElement;
+            console.log('Article URL:', this.url);
+        } else {
+            console.error('未找到 URL');
+        }
+    },
+
+    copyLink() {
+        navigator.clipboard.writeText(this.url)
+            .then(() => {
+                this.copied = true;
+                setTimeout(() => this.copied = false, 2000);
+            })
+            .catch(err => console.error('複製失敗', err));
+    }
+}));
