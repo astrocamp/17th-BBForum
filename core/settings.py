3dbfbdcc7463fe0c4ca0728644ceee554a5161ce
jjs <jenjingsu860630@gmail.com>
2024-09-12 19:53:52 +0800

    fix: 將點數渲染改至訪客底下

--- core/settings.py
+++ core/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "points.context_processors.user_points",
             ],
         },
     },
