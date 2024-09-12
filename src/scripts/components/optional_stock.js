71c7848e85346810e239c7415cfb97718b813661
erichsu <filafila0521@gmail.com>
2024-09-12 20:24:18 +0800

    refactor: 增加自選股插件

---
+++ src/scripts/components/optional_stock.js
@@ -0,0 +1,33 @@
+import Alpine from "alpinejs"
+
+Alpine.data("unlogined_prompt", (isAuthenticated) => ({
+    isVisible: false,
+    isReadonly: false,
+    isShow: false,
+    isAuthenticated: isAuthenticated,
+    articleContent: '',
+    commentContent: '',
+
+    toggleVisibility() {
+        this.isVisible = !this.isVisible;
+        this.isReadonly = !this.isReadonly;
+        this.isShow = !this.isShow;
+    },
+
+    submitForm() {
+        if (this.$refs.clearTextarea) {
+            this.$refs.clearTextarea.value = '';
+        }
+        this.isVisible = !this.isVisible;
+    },
+
+    submitCommentForm(event) {
+        if(this.commentContent.trim() === '') {
+            event.preventDefault();
+            alert("請輸入正確的留言訊息!!");
+        } else {
+            this.$refs.submitForm.requestSubmit();
+            this.commentContent = '';
+        }
+    },
+}))
