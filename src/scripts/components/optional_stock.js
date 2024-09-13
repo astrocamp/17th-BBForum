a65372b6445dcb2fc3c93dd31f5394ff3946f32c
erichsu <filafila0521@gmail.com>
2024-09-13 16:05:46 +0800

    feat: 增加可以自選股票和取消自選功能

--- src/scripts/components/optional_stock.js
+++ src/scripts/components/optional_stock.js
@@ -1,12 +1,66 @@
 import Alpine from "alpinejs"
 
 Alpine.data("optional_stock", (isAuthenticated) => ({
-    isVisible: false,
+    isShow: false,
+    isPick: false,
+    checkstatus: false,
     isAuthenticated: isAuthenticated,
+    // isOwnPost: pickUserId === currentStockId,
 
-    toggleVisibility(event) {
-        event.stopPropagation(); // 阻止事件冒泡
-        this.isVisible = !this.isVisible;
-        console.log("11111111111111");
+    async startPick(pickURL, unpickURL, checkPickURL, csrfToken) {
+        this.isShow = !this.isShow;
+
+        // await this.checkFollowStatus(checkPickURL);
+        console.log("-----------------------------")
+        console.log(pickURL)
+        console.log(unpickURL)
+        console.log(checkPickURL)
+        console.log(csrfToken)
+
+        const url = this.isPick ? unpickURL : pickURL;
+        const method = this.isPick ? 'DELETE' : 'POST';
+
+        fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify({}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(this.isPick)
+
+            // if (this.isPick) {
+            //     this.$refs.buttonStyle.style.background = '#ae2024';
+            //     this.$refs.buttonStyle.textContent = '追蹤';
+            //     this.$refs.buttonStyle.style.color = 'white';
+            // } else {
+            //     this.$refs.buttonStyle.style.background = '#0066CC';
+            //     this.$refs.buttonStyle.textContent = '取消追蹤';
+            //     this.$refs.buttonStyle.style.color = 'white';
+            // }
+
+            this.isPick = !this.isPick;
+        })
+        .catch(error => {
+            console.error('error:', error);
+        });
     },
+
+    async checkPickStatus(checkFollowURL) {
+        try {
+            const response = await fetch(checkFollowURL);
+            const data = await response.json();
+
+            if (data.is_following) {
+                this.isPick = true;
+            } else {
+                this.isPick = false;
+            }
+        } catch (error) {
+            console.error('error:', error);
+        }
+    }
 }));
