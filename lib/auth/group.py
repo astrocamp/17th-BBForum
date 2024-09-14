282c7c201847f7710d44c6fba6da6f4d4e73ce21
emma0620 <emmawang0620@gmail.com>
2024-09-14 17:29:48 +0800

    fix group.py bug

--- lib/auth/group.py
+++ lib/auth/group.py
@@ -1,16 +1,5 @@
 from django.contrib.auth.models import Group
 
-
-def assign_user_to_group(user, post_count):
-    group_level = post_count // 2
-    group_name = f"LV.{group_level}"
-    user.groups.clear()
-    group, created = Group.objects.get_or_create(name=group_name)
-    user.groups.add(group)
-
-
-from django.contrib.auth.models import Group
-
 from articles.models import Article
 from follows.models import FollowRelation
 
