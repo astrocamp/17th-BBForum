14a711b34a89e053365bd2ae3bf2d5b5d04088dd
emma0620 <emmawang0620@gmail.com>
2024-09-15 12:45:28 +0800

    fix: 修改group.py logical

--- lib/auth/group.py
+++ lib/auth/group.py
@@ -6,7 +6,7 @@ from follows.models import FollowRelation
 
 def assign_user_to_group(user, post_count, follow_count):
     # 計算發文數和追蹤者數的綜合結果
-    total_activity_count = (post_count // 10) + (follow_count // 10)
+    total_activity_count = (post_count // 10) + (follow_count // 20)
     group_name = f"LV.{total_activity_count}"
 
     group, created = Group.objects.get_or_create(name=group_name)
@@ -17,5 +17,5 @@ def assign_user_to_group(user, post_count, follow_count):
 # 將發文數和追蹤者數傳入
 def update_user_group(user):
     post_count = Article.objects.filter(user=user).count()
-    follow_count = FollowRelation.objects.filter(following=user).count()
+    follow_count = FollowRelation.objects.filter(follower=user).count()
     assign_user_to_group(user, post_count, follow_count)
