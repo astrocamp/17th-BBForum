8dfae862bb5ee232593d3b05258ed8d6935731a3
erichsu <filafila0521@gmail.com>
2024-09-16 20:52:49 +0800

    refactor: 移除不必要的檔案

--- follows/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import FollowRelation
-
-
-class FollowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FollowRelation
-        fields = ["follower", "following", "created_at"]
--- picks/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import UserStock
-
-
-class FollowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserStock
-        fields = ["user", "stock", "created_at"]
