064804d38248f1569d5e616366468a8c3b1679fd
jjs <jenjingsu860630@gmail.com>
2024-09-16 14:35:36 +0800

    fix: 刪除不必要的檔案

--- points/context_processors.py
+++
@@ -1,8 +0,0 @@
-from userprofiles.models import Profile
-
-
-def user_points(request):
-    if request.user.is_authenticated:
-        profile = Profile.objects.get(user=request.user)
-        return {"tot_point": profile.tot_point}
-    return {"tot_point": 0}
