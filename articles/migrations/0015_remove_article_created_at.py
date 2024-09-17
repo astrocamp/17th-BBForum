4c0fdef54c48fdbcb98e40d1935c85af3ad4c9cf
jjs <jenjingsu860630@gmail.com>
2024-09-17 13:32:38 +0800

    docs: 刪除不必要的註解

--- articles/migrations/0015_remove_article_created_at.py
+++ articles/migrations/0015_remove_article_created_at.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('articles', '0014_article_deleted_at'),
+        ("articles", "0014_article_deleted_at"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='article',
-            name='created_at',
+            model_name="article",
+            name="created_at",
         ),
     ]
