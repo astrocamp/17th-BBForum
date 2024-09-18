cc54233f99189ae80ba6bc7d40bd219fd812481e
erichsu <filafila0521@gmail.com>
2024-09-18 16:25:04 +0800

    refactor: 修改登出狀態不抓取股票資訊

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth.models import AnonymousUser
 from django.db.models import Count, Exists, OuterRef
 from django.shortcuts import render
 
@@ -63,9 +64,12 @@ def index(req):
                 req, "pages/main_page/_articles_list.html", {"articles": articles}
             )
 
-    pick_stocks = UserStock.objects.filter(user=req.user).values_list(
-        "stock__security_code", flat=True
-    )
+    if isinstance(req.user, AnonymousUser):
+        pick_stocks = []
+    else:
+        pick_stocks = UserStock.objects.filter(user=req.user).values_list(
+            "stock__security_code", flat=True
+        )
 
     random_five_tags = (
         IndustryTag.objects.filter(industry="半導體業")
