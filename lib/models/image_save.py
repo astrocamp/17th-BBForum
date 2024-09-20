6be39e3bdc9d92f678addf29fd8c69d5468dc45b
emma0620 <emmawang0620@gmail.com>
2024-09-20 14:47:00 +0800

    feat: 增加圖片檔案大於500kb便壓縮改為webp檔案

--- lib/models/image_save.py
+++ lib/models/image_save.py
@@ -1,3 +1,10 @@
+import io
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from PIL import Image
+
+
 class ImageSaveMixin:
     def save(self, *args, **kwargs):
         if self.pk:
@@ -10,4 +17,16 @@ class ImageSaveMixin:
                     print("No old photo to delete or no change in photo.")
             except self.__class__.DoesNotExist:
                 print("Old instance does not exist.")
+
+        # 在保存之前檢查圖片大小
+        if self.photo:
+            print("重新壓縮照片檔案")
+            img = Image.open(self.photo)
+            img_size = self.photo.size
+            if img_size > 500 * 1024:
+                img_io = io.BytesIO()
+                img.save(img_io, format="WEBP", quality=80)
+                img_io.seek(0)
+                self.photo.save(self.photo.name, ContentFile(img_io.read()), save=False)
+
         super().save(*args, **kwargs)
