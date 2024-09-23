8559d311b2cb98b35516d27596ad0db3a89e6a53
erichsu <filafila0521@gmail.com>
2024-09-23 11:52:27 +0800

    refactor: 在大盤頁面增加有tag股票的所有文章

--- stockpages/views.py
+++ stockpages/views.py
@@ -1,7 +1,6 @@
-import os
-import subprocess
 from datetime import datetime
 
+from django.db.models import Count, Exists, OuterRef
 from django.shortcuts import get_object_or_404, render
 
 from articles.models import Article, IndustryTag
@@ -14,6 +13,23 @@ def stock_data_twii(req):
     latest_price, percent_change, price_change, trading_units = get_stock_data("^TWII")
     current_time = datetime.now().strftime("%m/%d %H:%M")
 
+    if req.user.is_authenticated:
+        profile = get_object_or_404(Profile, user=req.user)
+        user_img = profile.user_img
+    else:
+        user_img = None
+        
+    subquery = Article.objects.filter(liked=req.user.pk, id=OuterRef("pk")).values("pk")
+
+    articles = (
+        Article.objects.filter(stock__isnull=False)
+        .distinct()
+        .annotate(user_liked=Exists(subquery), like_count=Count("liked"))
+        .distinct()
+        .order_by("-id")
+        .prefetch_related("stock")
+    )
+
     return render(
         req,
         "pages/market_index/market_index.html",
@@ -24,22 +40,16 @@ def stock_data_twii(req):
             "percent_change": percent_change,
             "current_time": current_time,
             "twii": True,
+            "user_img": user_img,
+            "articles": articles,
         },
     )
 
 
 def stock_data(req, id):
-    # if os.name == "nt":  # Windows
-    #     python_executable = r".venv\Scripts\python.exe"
-    # else:  # macOS/Linux
-    #     python_executable = ".venv/bin/python"
-
-    # subprocess.Popen([python_executable, "stockpages/stockdash.py", str(id)])
-
     stock = get_object_or_404(IndustryTag, security_code=id)
     articles = Article.objects.filter(stock=id).order_by("-id")
 
-    # Get stock price and percentage change
     latest_price, percent_change, price_change, trading_units = get_stock_data(id)
     current_time = datetime.now().strftime("%m/%d %H:%M")
 
