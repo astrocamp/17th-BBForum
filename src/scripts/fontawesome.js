b1e637004432ee97f0b625b3f8bfd45713456b40
jjs <jenjingsu860630@gmail.com>
2024-09-23 16:21:16 +0800

    fix: 修改fontawesome多餘的東西

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -1,9 +1,9 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
 import { faBookmark as fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus, faAngleRight, faSortDown, faSortUp, faEllipsis, faShare, faXmark, faP, faCircle, faMoneyBillTrendUp, faCommentDots, faChartLine, faUser, faList } from "@fortawesome/free-solid-svg-icons"
-import { faBookmark as farBookmark, faComment, faThumbsUp, faList as farList } from "@fortawesome/free-regular-svg-icons"
+import { faBookmark as farBookmark, faComment, faThumbsUp} from "@fortawesome/free-regular-svg-icons"
 import Alpine from "alpinejs"
 
-library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus, faAngleRight, faSortDown, faSortUp, faEllipsis, faShare, faComment, faThumbsUp, faXmark, faP, faCircle, faMoneyBillTrendUp, faCommentDots, faChartLine, faUser, faList, farList)
+library.add(farBookmark, fasBookmark, faMagnifyingGlass, faBell, faCirclePlus, faBookmark, faMessage, faPlus, faAngleRight, faSortDown, faSortUp, faEllipsis, faShare, faComment, faThumbsUp, faXmark, faP, faCircle, faMoneyBillTrendUp, faCommentDots, faChartLine, faUser, faList)
 dom.i2svg()
 
 document.addEventListener("htmx:afterRequest", () => {
