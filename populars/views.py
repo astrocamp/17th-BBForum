53186c098ce898096e0ede74962ddc5b92fefe49
楊聯崧 <a19901229s@gmail.com>
2024-09-23 18:34:55 +0800

    refactor: 修改取前五名被追蹤數量，如果當中有相同追蹤數量的使用者只佔一個名額

--- populars/views.py
+++ populars/views.py
@@ -23,19 +23,34 @@ def popular_stocks(request):
 
 
 def popular_students(req):
-    top_users = (
-        FollowRelation.objects
-        .values('following')
-        .annotate(follower_count=Count('follower'))
-        .order_by('-follower_count')[:5]
+    follow_counts = (
+        FollowRelation.objects.values("following")
+        .annotate(follower_count=Count("follower"))
+        .order_by("-follower_count")
     )
-    
-    top_user_data = {user['following']: user['follower_count'] for user in top_users}
+
+    unique_counts = []
+    for follow in follow_counts:
+        if follow["follower_count"] not in unique_counts:
+            unique_counts.append(follow["follower_count"])
+
+    top_counts = unique_counts[:5]
+
+    top_users = follow_counts.filter(follower_count__in=top_counts)
+
+    if len(top_counts) > 5:
+        sixth_count = top_counts[5]
+        additional_users = follow_counts.filter(follower_count=sixth_count)
+        top_users = list(top_users) + list(additional_users)
+
+    top_user_data = {user["following"]: user["follower_count"] for user in top_users}
     top_user_ids = list(top_user_data.keys())
 
     subquery = Article.objects.filter(liked=req.user.pk, id=OuterRef("pk")).values("pk")
-    collect = Article.objects.filter(collectors=req.user.pk, id=OuterRef("pk")).values("pk")
-    
+    collect = Article.objects.filter(collectors=req.user.pk, id=OuterRef("pk")).values(
+        "pk"
+    )
+
     articles = (
         Article.objects.filter(user_id__in=top_user_ids)
         .annotate(
@@ -54,7 +69,12 @@ def popular_students(req):
         )
         .order_by("-follower_count")
     )
-    return render(req, "popular_pages/popular_students/popular_students.html", {"articles": articles})
+    return render(
+        req,
+        "popular_pages/popular_students/popular_students.html",
+        {"articles": articles},
+    )
+
 
 def popular_answers(request):
     if request.user.is_authenticated:
