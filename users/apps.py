f60557b77d45cf175348cbf49b75f00ce6df4f80
emma0620 <emmawang0620@gmail.com>
2024-09-23 11:01:54 +0800

    fix: group.py apps logical

--- users/apps.py
+++ users/apps.py
@@ -1,24 +1,23 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate, post_save
+from django.db.models.signals import post_migrate
 
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
+    def ready(self):
+        from django.contrib.auth.models import Group, User
+        from django.db.models.signals import post_save
 
-def ready(self):
-    from django.contrib.auth.models import Group, User
-    from django.db.models.signals import post_save
-
-    post_migrate.connect(create_groups, sender=self)
-
-    post_save.connect(assign_default_group, sender=User)
+        post_migrate.connect(create_groups, sender=self)
+        post_save.connect(assign_default_group, sender=User)
 
 
 def create_groups(sender, **kwargs):
     from django.contrib.auth.models import Group
 
+    print("creategroups0~49")
     for i in range(50):
         group_name = f"LV.{i}"
         Group.objects.get_or_create(name=group_name)
@@ -27,6 +26,7 @@ def create_groups(sender, **kwargs):
 def assign_default_group(sender, instance, created, **kwargs):
     from django.contrib.auth.models import Group
 
+    print("creategroup LV0")
     if created:
         default_group, created = Group.objects.get_or_create(name="LV.0")
         instance.groups.add(default_group)
