8d0c06c06a0e0801afa6b92ea4b12d387a5d20ef
KANG <k306221@gmail.com>
2024-09-25 13:32:59 +0800

    fix: 可以導向錯誤頁面，但頁面尚未設計

--- src/scripts/components/stock_search.js
+++ src/scripts/components/stock_search.js
@@ -3,29 +3,31 @@ import Alpine from "alpinejs";
 Alpine.data("stock_search", () => ({
     query: '',
     suggestions: [],
-    selectedStockCode: null, 
+    selectedStockCode: null,
     searchStocks() {
         if (this.query.length > 0) {
             fetch(`/search/?q=${this.query}`)
                 .then(response => response.json())
                 .then(data => {
-                    this.suggestions = data.slice(0, 5);  
+                    this.suggestions = data.slice(0, 5);
                 });
         } else {
             this.suggestions = [];
         }
     },
     selectStock(securityCode, name) {
-        this.query = name;  // 保留用户输入的名称
-        this.suggestions = [];  
-        this.selectedStockCode = securityCode; 
+        this.query = name;
+        this.suggestions = [];
+        this.selectedStockCode = securityCode;
     },
     navigateToStock() {
-        const matchedStock = this.suggestions.find(suggestion => 
+        const matchedStock = this.suggestions.find(suggestion =>
             suggestion.name === this.query || suggestion.security_code === this.query
         );
-        const securityCode = matchedStock ? matchedStock.security_code : this.selectedStockCode; 
-        if (securityCode) {
+        const securityCode = matchedStock ? matchedStock.security_code : this.selectedStockCode;
+        if (!securityCode) {
+            window.location.href = 'stock_notfound/';
+        } else {
             window.location.href = `/stocks/${securityCode}`;
         }
     }
